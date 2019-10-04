debd8df88a8b504a7704315c87bff807c2eacd56
Rhuan Carlos <pf.rhuan@gmail.com>
2019-10-04 09:42:07 -0300

    Adicionando alguns exercicios feitos de c#

---
+++ Data-struct/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dias = DateTime.Now.Day;
+            int meses = DateTime.Now.Month;
+            int anos = DateTime.Now.Month;
+
+            if (dias % 2 == 0)
+            {
+                Console.WriteLine($"Hoje é dia: {dias} \nHoje é um dia par.");
+            } else
+            {
+                Console.WriteLine($"Hoje é dia: {dias} \nHoje é um dia impar.");
+            }
+
+            ShowDate data;
+
+            data = new ShowDate() { dia = 10, mes = meses, ano = anos};
+
+            if (dias < 10)
+            {
+                Console.WriteLine($"Data: 0{data.dia}/{data.mes}/{data.ano}");
+            }
+            else
+            {
+                Console.WriteLine($"Data: {data.dia}/{data.mes}/{data.ano}");
+            }
+            
+        }
+        struct ShowDate
+        {
+            public int dia;
+            public int mes;
+            public int ano;
+        }
+    }
+}
